0ef06caa4a5ac282185833adf8f6c0fbc8f0a9cc
Alexander Swensen <alex.swensen@gmail.com>
2020-11-05 00:13:12 -0600

    add test for vcs

---
+++ src/utils/vcs.test.ts
@@ -0,0 +1,7 @@
+import { VCSProvider } from "./vcs";
+describe("vcs enum", () => {
+  it("should evaluate to the correct string", () => {
+    expect(VCSProvider.github).toEqual("gh");
+    expect(VCSProvider.bitbucket).toEqual("bb");
+  });
+});
